5fa67af7d1d40d46a887ebd75aecc550c3f81365
Fabiano Rosa <fabianorosa1@gmail.com>
2026-02-21 18:50:34 +0100

    fix path

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -5,7 +5,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://fabianorosa.me',
-  base: '/fabianorosa1',  
+  base: '/',  
   output: 'static',
   trailingSlash: 'always',
   build: {
