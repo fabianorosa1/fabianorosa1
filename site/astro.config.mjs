cc60544d847d41c281cce74208661d3089acbec8
Fabiano Rosa <fabianorosa1@gmail.com>
2026-02-21 17:24:15 +0100

    Update site

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
+import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [tailwind()],
   site: 'https://fabianorosa.me',
   base: '/fabianorosa1',
   output: 'static',
